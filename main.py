fa5c6baa5ab4cb702d4ddeba1bf3260d997d34c9
27stephentran <121224237+27stephentran@users.noreply.github.com>
2024-05-27 12:27:00 +0000

    Day 02 Update

--- main.py
+++ main.py
@@ -7,6 +7,11 @@ app  = Flask(__name__)
 def index():
     return render_template("index.html")
 
+@app.route("/input_page")
+def input_page():
+    return render_template("input_page.html")
+
+
 if __name__ == "__main__":
     app.run()
     
