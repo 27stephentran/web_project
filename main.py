2901b04f7d1e0775e53fb5c1a471bbbbc9474cac
27stephentran <121224237+27stephentran@users.noreply.github.com>
2024-06-24 12:39:12 +0000

    Day 06 Update

--- main.py
+++ main.py
@@ -12,20 +12,95 @@ def input_page():
     if request.method == "POST":
         days_off = request.form.get("d-off")
         if request.form.get("l-travel") == "True":
+            long_travel = "True"
             long_travel_option = request.form.get("long-travel-options")
             other_interest = request.form.get("long-travel-other")
             budget = request.form.get("budget")
             people = request.form.get("people")
-            print(days_off, long_travel_option, other_interest, budget, people)
+            plan = generate_plan(days_off, long_travel, long_travel_option, other_interest, budget, people)
+            print(days_off, long_travel, long_travel_option, other_interest, budget, people)
+            
             
         else:
+            long_travel = "False"
             short_travel_option = request.form.get("short-travel-options")
             other_interest = request.form.get("short-travel-other")
             budget = request.form.get("budget")
             people = request.form.get("people")
-            print(days_off, short_travel_option, other_interest, budget, people)
-            
+            plan = generate_plan(days_off, long_travel, short_travel_option, other_interest, budget, people)
+        
+        return render_template("recommendation.html", recommendations = plan)
     return render_template("input_page.html")
+
+@app.route("/recommendation")
+def recomendation():
+    return render_template("recommendation.html")
+
+def generate_plan(days_off, long_travel, travel_option, other_interest, budget, people):
+    html_content = '''
+    <h1> Here is our recomendation based on your choices</h1>
+    '''
+
+    long_travel_plan = {
+        "Nha-trang" : {
+            "Day 01": [["LONG SON PAGODA WITH WHITE BUDDHA"], 
+                       ["HON CHONG ROCKS"], 
+                       ["NATIONAL OCEANOGRAPHIC MUSEUM"]],
+            "Day 02": [["NHA TRANG CATHEDRAL"], 
+                       ["THE BEACHE"], 
+                       ["FESTIVALS IN NHA TRANG"]]
+        },
+        "Option-2" : {
+            "Day 01": [["Activity 01"],
+                       ["Activity 02"],
+                       ["Activity 03"]],
+
+            "Day 02" : [["Activity 04"],
+                       ["Activity 05"],
+                       ["Activity 06"]],
+        },
+        "Option-3" : {
+            "Day 01": [["Activity 01"],
+                       ["Activity 02"],
+                       ["Activity 03"]],
+
+            "Day 02" : [["Activity 04"],
+                       ["Activity 05"],
+                       ["Activity 06"]],
+        }
+    }
+    if long_travel == "True":
+        place = long_travel_plan[travel_option] 
+        activities = []
+        for day in place:
+            activities.append(place[day])
+        
+    html_content += '<table border = "1" color = "red">'
+    for day in range(int(days_off)):
+        html_content += f'''
+        <tr><th colspan= "3"> Day {day+1}</th></tr>
+        '''
+
+        parts_of_day = ["Morning", "Afternoon", "Evening"]
+
+        for part in parts_of_day:
+            html_content += f'<tr><td>{part}:</td>'
+
+            try: 
+                activity = activities[0][parts_of_day.index(part)][0]
+                html_content += f'<td colspan = "2">{activity}</td>'
+                
+            except:
+                break
+
+        html_content += "</tr>"
+
+    html_content += "</table>"
+
+    return html_content
+
+
+
 
 
 
